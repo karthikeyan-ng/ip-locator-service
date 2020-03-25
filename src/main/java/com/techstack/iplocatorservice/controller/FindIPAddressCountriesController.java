912b55f7e5392682e929cf1f5878f1043ffcef40
Karthikeyan Nithiyanandam <karthikeyan.ng.jobs@gmail.com>
2020-03-26 00:04:12 +0530

    The endpoint should accept list of IP addresses passed as request parameters

--- src/main/java/com/techstack/iplocatorservice/controller/FindIPAddressCountriesController.java
+++ src/main/java/com/techstack/iplocatorservice/controller/FindIPAddressCountriesController.java
@@ -1,12 +1,15 @@
 package com.techstack.iplocatorservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FindIPAddressCountriesController {
 
     @GetMapping("/northcountries")
-    public void getCountries() {
+    public void getCountries(@RequestParam(name = "ip") List<String> ipAddresses) {
     }
 }
