61036d45d69edc354e98e56eccca4c95dbb5d99c
Karthikeyan Nithiyanandam <karthikeyan.ng.jobs@gmail.com>
2020-03-25 23:48:35 +0530

    Created a application through start.spring.io and committed the same as Initial Commit.

---
+++ src/test/java/com/techstack/iplocatorservice/IpLocatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.iplocatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLocatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
