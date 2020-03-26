ea1273ec9972981c57ffacafc79e41cba8f13d82
Karthikeyan Nithiyanandam <karthikeyan.ng.jobs@gmail.com>
2020-03-26 23:57:16 +0530

    The endpoint should accept at least 1 and maximum 5 ip addresses. For this added a Java Size validation which contain min and max with appropriate error message.

--- src/main/java/com/techstack/iplocatorservice/controller/FindIPAddressCountriesController.java
+++ src/main/java/com/techstack/iplocatorservice/controller/FindIPAddressCountriesController.java
@@ -1,15 +1,23 @@
 package com.techstack.iplocatorservice.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.util.List;
 
+@Validated
 @RestController
 public class FindIPAddressCountriesController {
 
     @GetMapping("/northcountries")
-    public void getCountries(@RequestParam(name = "ip") List<String> ipAddresses) {
+    public void getCountries(@Valid
+                            @RequestParam(name = "ip")
+                            @Size(min = 1, max = 5, message = "User can input minimum 1 and maximum 5 IP addresses at a time")
+                                         List<String> ipAddresses) {
     }
+
 }
