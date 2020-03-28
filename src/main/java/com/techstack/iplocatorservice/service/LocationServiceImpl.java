294cf341d72a805624a1a28e2af5066a08c41ade
Karthikeyan Nithiyanandam <karthikeyan.ng.jobs@gmail.com>
2020-03-28 19:04:04 +0530

    Process the response returned from IP Vigilante API.

--- src/main/java/com/techstack/iplocatorservice/service/LocationServiceImpl.java
+++ src/main/java/com/techstack/iplocatorservice/service/LocationServiceImpl.java
@@ -5,10 +5,10 @@ import com.techstack.iplocatorservice.service.external.api.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -17,18 +17,31 @@ public class LocationServiceImpl implements LocationService {
     private final IpVigilanteService ipVigilanteService;
 
     @Override
-    public Map<String, List<String>> getCountries(List<String> ipAddresses) {
+    public Map<String, List<String>> getCountries(final List<String> ipAddresses) {
+
+        List<Response> responses = processIpAddresses(ipAddresses);
+        Map<String, List<String>> filteredCountries = validateAndGetCountries(responses);
+        return filteredCountries;
+    }
+
+    public List<Response> processIpAddresses(final List<String> ipAddresses) {
 
         List<Response> responses = ipVigilanteService.getIpAddressDetails(ipAddresses);
+        return responses;
+    }
 
-        //TODO: Further process
+    public Map<String, List<String>> validateAndGetCountries(final List<Response> responses) {
+
+        List<String> countries =
+                responses.stream()
+                        .filter(e -> e.getStatus().equals("success"))
+                        .map(e -> e.getCountryInfo().getCountryName())
+                        .distinct()
+                        .collect(Collectors.toList());
 
         Map<String, List<String>> countriesMap = new HashMap<>();
-        List<String> countries = new ArrayList<>();
-        countries.add("Colombia");
-        countries.add("Japan");
-        countries.add("United States");
         countriesMap.put("northcountries", countries);
         return countriesMap;
     }
+
 }
